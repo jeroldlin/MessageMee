fc47c97dfdc9cec8308039cea794db582ff1b99f
jeroldlin <jeroldlin@gmail.com>
2015-12-25 16:15:35 -0600

    1

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
    <title>Message Mee - send SMS on the web</title>
    <link type="text/css" href="bootstrap.min.css" rel="stylesheet" />
    <link type="text/css" href="Style.css" rel="stylesheet" />
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+   <script src="jquery.min.js"></script>
+   <script src="bootstrap.min.js"></script>
    <script src="jquery.maskedinput.js" type="text/javascript"></script>
    <script src="WebDesign.js" type="text/javascript"></script>
    <script src="webFunction.js" type="text/javascript"></script>
